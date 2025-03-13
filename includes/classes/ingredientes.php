607c2bf4402b3f86d887597093cb7eab05e53615
Bruno <dolareeu@gmail.com>
2025-03-13 19:18:40 -0300

    'testefinalizado'

--- includes/classes/ingredientes.php
+++ includes/classes/ingredientes.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class ingrediente {
     public $ingrediente;
@@ -14,21 +13,4 @@ class ingrediente {
 
     }
 }
-=======
-<?php
-class ingrediente {
-    public $ingrediente;
-
-    function getIngrediente(){
-        return $this->$ingrediente;
-    }
-    function setIngrediente($ing){
-        $this->$ingrediente = $ing;
-    }
-
-    function cadastrar($ingrediente){
-
-    }
-}
->>>>>>> be628867dd9ad8e9835bf6fb65f8c03f12552ca2
 ?>
