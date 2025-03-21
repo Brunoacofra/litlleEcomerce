6f7693cd8d8f55b192be4c426da2b0135f911d1c
Bruno <dolareeu@gmail.com>
2025-03-20 22:28:23 -0300

    select dinamico com id

--- includes/funcoes/function.js
+++ includes/funcoes/function.js
@@ -1,20 +1,27 @@
 function adicionar(){
     var  a = document.getElementById("campos");
+    qtd = a.getElementsByTagName("select").length;
     elemento = document.createElement("select");
+    elemento.id = "select_", qtd+1;
     var xrh = new XMLHttpRequest();
     xrh.open("POST","buscaIngredientes.php",true);
     xrh.onload  = function (){
-       alert("resultado: "+this.responseText);
+        resultado = this.responseText;
+        op = document.createElement("option");
+        op.innerText = resultado;
+        var pai = document.querySelectorAll("#campos select")
+        a.appendChild(elemento);
+        elemento.appendChild(op);
     }
     xrh.send();
-    a.append(elemento);
+    
 }
 function remover(){
     var  a = document.getElementById("campos");
     qtd = a.getElementsByTagName("select").length;
     if(qtd>=1){
-        var  a = document.querySelector("#campos select");
-        a.remove();
+        var  a = document.querySelectorAll("#campos select");
+        a[a.length-1].remove();
     }
     else
     {
